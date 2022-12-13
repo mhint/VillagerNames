760c09dffac8eafa48a14a4c3bf760ea683343e5
Morris Hinton <mhinton294@gmail.com>
2022-12-13 00:59:51 -0800

    Add Gradle files

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("java-library")
+}
+
+group = "com.adenark"
+version = "1.0-SNAPSHOT"
+description = ""
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.19.2-R0.1-SNAPSHOT")
+}
+
+tasks {
+    compileJava {
+        options.encoding = Charsets.UTF_8.name()
+        options.release.set(17)
+    }
+    javadoc {
+        options.encoding = Charsets.UTF_8.name()
+    }
+    processResources {
+        filteringCharset = Charsets.UTF_8.name()
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,8 @@
+rootProject.name = "VillagerNames"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://repo.papermc.io/repository/maven-public/")
+    }
+}
