6c692e48f3fa11f79c59750e1251c4f7d1a23f73
Morris Hinton <mhinton294@gmail.com>
2022-12-13 03:03:08 -0800

    Update HelpCmd.java

--- src/main/java/com/adenark/villagernames/command/HelpCmd.java
+++ src/main/java/com/adenark/villagernames/command/HelpCmd.java
@@ -7,16 +7,18 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
 public class HelpCmd implements CommandExecutor {
-
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
-        commandSender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&a[Plugin]: &r" + VillagerNames.getInstance().getName()));
+        commandSender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&a[Plugin]: &r" +
+            VillagerNames.getInstance().getName()));
         commandSender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&a[Config]"));
-        commandSender.sendMessage(ChatColor.translateAlternateColorCodes('&', "Debug mode: " + VillagerNames.DEBUG));
-        commandSender.sendMessage(ChatColor.translateAlternateColorCodes('&', "Names loaded: " + VillagerNames.NAMES.size()));
-        commandSender.sendMessage(ChatColor.translateAlternateColorCodes('&', "Chance: : " + VillagerNames.NAME_POTENTIAL * 100 + "%"));
+        commandSender.sendMessage(ChatColor.translateAlternateColorCodes('&', "Debug mode: " +
+            VillagerNames.DEBUG));
+        commandSender.sendMessage(ChatColor.translateAlternateColorCodes('&', "Names loaded: " +
+            VillagerNames.NAMES.size()));
+        commandSender.sendMessage(ChatColor.translateAlternateColorCodes('&', "Chance: : " +
+            VillagerNames.NAME_POTENTIAL * 100 + "%"));
         commandSender.sendMessage("");
         return true;
     }
-
 }
